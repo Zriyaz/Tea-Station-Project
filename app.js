3950f896ff1963836d789408ee27ec2dd763d3ce
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-30 13:28:42 +0000

    //commit

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 // show nav
-/*navBtn.addEventListener("click", () => {
+navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
 });
 // close nav
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
-});*/
+});
